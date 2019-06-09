507abeb007ee18728ab0edbb4d53aebd787e4a9b
Wavyline <hannawavy@gmail.com>
2019-06-09 16:58:04 +0300

    feature(front page) gallery style

--- wp-content/themes/painteco/template-parts/front_page_gallery.php
+++ wp-content/themes/painteco/template-parts/front_page_gallery.php
@@ -1,7 +1,10 @@
 <div class="container tools">
     <div class="row">
-        <div class="col-sm-12">
-            gallery
-        </div>
+        <div class="col-sm-4"><img src="" alt="gallery" style="width: 100%"></div>
+        <div class="col-sm-4"><img src="" alt="gallery" style="width: 100%"></div>
+        <div class="col-sm-4"><img src="" alt="gallery" style="width: 100%"></div>
+        <div class="col-sm-4"><img src="" alt="gallery" style="width: 100%"></div>
+        <div class="col-sm-4"><img src="" alt="gallery" style="width: 100%"></div>
+        <div class="col-sm-4"><img src="" alt="gallery" style="width: 100%"></div>
     </div>
 </div>
