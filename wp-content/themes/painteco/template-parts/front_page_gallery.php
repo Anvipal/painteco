68c64bf97f428aedcb18c326827b4ee219b08b71
alexander_anvi <a.vizniuk@platonline.com>
2019-06-09 17:40:40 +0300

    front page gallery from post

--- wp-content/themes/painteco/template-parts/front_page_gallery.php
+++ wp-content/themes/painteco/template-parts/front_page_gallery.php
@@ -1,10 +1,12 @@
-<div class="container tools">
+<!-- FRONT GALLERY START -->
+<div class="container tools front-gallery">
+    <?php $images = get_field('gallery_images', 3594); ?>
+
     <div class="row">
-        <div class="col-sm-4"><img src="" alt="gallery" style="width: 100%"></div>
-        <div class="col-sm-4"><img src="" alt="gallery" style="width: 100%"></div>
-        <div class="col-sm-4"><img src="" alt="gallery" style="width: 100%"></div>
-        <div class="col-sm-4"><img src="" alt="gallery" style="width: 100%"></div>
-        <div class="col-sm-4"><img src="" alt="gallery" style="width: 100%"></div>
-        <div class="col-sm-4"><img src="" alt="gallery" style="width: 100%"></div>
+        <?php foreach ($images as $image) : ?>
+        <div class="col-sm-4"><img src="<?php echo $image['url'] ?>" alt="<?php echo $image['alt']; ?>" style="width: 100%"></div>
+        <?php endforeach; ?>
     </div>
+
 </div>
+<!-- FRONT GALLERY END -->
