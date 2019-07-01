327bf22dbf5cdbd2c20d292d44231258ffd2e173
Wavyline <hannawavy@gmail.com>
2019-07-01 21:07:06 +0300

    feature(footer) subscribe form

--- wp-content/themes/painteco/template-parts/footer_subscribe.php
+++ wp-content/themes/painteco/template-parts/footer_subscribe.php
@@ -1,10 +1,11 @@
 <section class="footer-subscribe">
     <div class="container">
         <form action="" class="form-horizontal">
+            <input type="hidden" name="form_name" value="Jaunumu forma">
             <div class="form-group">
                 <label for="" class="col-sm-5">Subscribe to our newsletter:</label>
                 <div class="col-sm-4">
-                    <input type="email" class="form-control" placeholder="my.email@gmail.com" />
+                    <input type="email" name="subscribe_email" class="form-control" placeholder="my.email@gmail.com" />
                 </div>
                 <div class="col-sm-2">
                     <button type="submit" class="btn btn-success btn-block">Send</button>
