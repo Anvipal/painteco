af97a4c67cd91f3a9d5805f88a3fd2c1697f95b2
alexander_anvi <a.vizniuk@platonline.com>
2019-07-05 21:05:01 +0300

    hide

--- wp-content/themes/painteco/template-parts/content-blog-list-item.php
+++ wp-content/themes/painteco/template-parts/content-blog-list-item.php
@@ -25,7 +25,7 @@
 					the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 				}
 
-			if ( 'post' === get_post_type() ) : ?>
+			if ( 'post' === get_post_type() && !in_category(['padomi', 'sovety', 'tips'])) : ?>
 			<div class="entry-meta">
 				<?php painteco_posted_on(); ?>
 			</div><!-- .entry-meta -->
