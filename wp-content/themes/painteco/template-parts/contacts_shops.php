a15a2da565ae4019f8c7ac7011ff8bef3445be04
alexander_anvi <a.vizniuk@platonline.com>
2019-07-09 21:15:42 +0300

    translations for contacts_shops

--- wp-content/themes/painteco/template-parts/contacts_shops.php
+++ wp-content/themes/painteco/template-parts/contacts_shops.php
@@ -1,9 +1,10 @@
 <div class="contacts-shops">
     <div class="container">
-        <h2 class="page-section-title text-uppercase text-center">where to buy</h2>
+        <h2 class="page-section-title text-uppercase text-center"><?php echo __( 'Kur nopirkt', 'painteco' ) ?></h2>
         <div class="contacts-shops_list" id="shops_list"></div>
         <div class="text-center">
-            <div class="contacts_showall btn btn-success" id="contacts_showall"><span>Show All</span><span style="display: none">Hide</span></div>
+            <div class="contacts_showall btn btn-success" id="contacts_showall"><span><?php echo __( 'Parādīt visu',
+						'painteco' ) ?></span><span style="display: none"><?php echo __('Slēpt','painteco') ?></span></div>
         </div>
     </div>
 </div>
