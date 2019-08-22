8ea982d0bc8d1e773b89e74935e77e560828fb5f
alexander_anvi <a.vizniuk@platonline.com>
2019-08-22 23:12:07 +0300

    https://trello.com/c/g1cH5mFb

---
+++ wp-content/themes/painteco/template-thankyou.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 22.08.2019
+ * Time: 23:06
+ */
+
+get_header();
+
+echo __('Paldies, ka pieteicātiem mūsu jaunumiem!', 'painteco');
+
+get_footer();
